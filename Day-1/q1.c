91ce7a2e21704207d2a6cde9119cb7195bf95cab
supriyaaav <25114cn248glbitm.ac.in>
2026-06-05 23:27:10 +0530

    Day-1 uploadaded

--- Day-1/q1.c
+++ Day-1/q1.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
 int main()
 {
-    printf("Hello, World!");
+   int n,sum;
+   printf("Enter a number:");
+
+   scanf("%d",&n);
+    sum=n*(n+1)/2 ;
+    printf("the sum of first %d natural number is %d",n,sum);
+    
     return 0;
 }
