87c9fd9f1d221b3f1d4989a470135ed92a7a75be
supriyaaav <25114cn248glbitm.ac.in>
2026-06-08 13:02:29 +0530

    updated q1.c

---
+++ Day-3/q1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+  printf("Enter a number:");
+  scanf("%d",&n);
+   for(i=2;i<n;i++)
+   {
+    if(n%i==0)
+    {
+        printf("Not a prime number");
+        return 0;
+    }
+       
+   }
+    printf("Prime number");
+    return 0;
+
+}
