bb0df9a27a348ce290b1a505cee27944a839d8a7
supriyaaav <25114cn248glbitm.ac.in>
2026-06-10 08:47:30 +0530

    updated q1.c

---
+++ Day-9/q1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,n;
+
+    printf("Enter n:");
+    scanf("%d",&n);
+
+    for (i = 1; i<=n ;i++)
+    {
+        for (j=i ; j<=n ; j++)
+        {
+            printf("*",j);
+
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
