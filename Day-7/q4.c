b76de976676743713d3bf36c10429e6ea95eab66
supriyaaav <25114cn248glbitm.ac.in>
2026-06-10 08:03:49 +0530

    updated q4.c

---
+++ Day-7/q4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+    void reverse(int n)
+    {
+     if(n==0)
+     
+     return;
+
+     printf("%d",n%10);
+     reverse (n/10);
+          
+    }
+
+    int main()
+    {
+        int n;
+        printf("Enter a number :");
+        scanf("%d",&n);
+
+        printf("Reverse number :");
+        reverse(n);
+
+        return 0;
+
+    }
