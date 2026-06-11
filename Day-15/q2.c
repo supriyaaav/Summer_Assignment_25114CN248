bcbbc4fd57b84365be5c118c95e519a52d7e016d
supriyaaav <25114cn248glbitm.ac.in>
2026-06-11 17:25:22 +0530

    updated q2.c

---
+++ Day-15/q2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    int i ,n  , temp;
+  
+
+    printf("Enter size of an array:");
+    scanf("%d",&n);
+    
+      int a[n];
+
+    printf("Enter element :");
+
+    for(i =0 ; i<n ; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    temp = a[0];
+
+    for(i =0 ; i<n-1 ; i++)
+    {
+        a[i] = a[i+1];
+        
+    }
+      a[n-1] = temp ;
+
+      printf("Left rotation array:%d\n");
+
+      for(i=0 ; i<n; i++)
+
+      {
+
+    printf("%d", a[i]);
+      }
+    return 0;
+
+
+}
