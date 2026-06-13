dd08306f423b0c0dab47da9b9140ae3259aba093
supriyaaav <25114cn248glbitm.ac.in>
2026-06-13 15:16:56 +0530

    updated q1.c

---
+++ Day-19/q1.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int i , j ,n;
+
+    printf("Enter size :");
+    scanf("%d" , &n);
+
+    int a[n][n];
+    int b[n][n];
+    int c[n][n];
+
+    printf("Enter First matrix element:\n");
+
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+            scanf("%d" , &a[i][j]);
+        }
+    }
+    printf("Enter second matrix element :\n");
+
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+            scanf("%d" , &b[i][j]);
+        }
+    }
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+            c[i][j] = a[i][j] + b[i][j];
+        }
+    }
+    printf("Addition of matrices :\n");
+    for(i=0 ; i<n ; i++)
+    {
+        for(j=0 ; j<n ; j++)
+        {
+                printf("%d \n",c[i][j]);
+        }
+    }
+
+
+    return 0 ;
+
+}
