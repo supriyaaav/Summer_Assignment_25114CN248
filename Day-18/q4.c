fba02ea6c5ac7c13045bded5fbca73f61387d4e5
supriyaaav <25114cn248glbitm.ac.in>
2026-06-13 14:33:36 +0530

    updated q4.c

---
+++ Day-18/q4.c
@@ -0,0 +1,41 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+
+    printf("Enter size :");
+    scanf("%d" ,&n);
+
+    int a[n];
+
+    printf("Enter element :\n");
+
+    int temp;
+    for(i=0 ; i<n ; i++)
+    {
+        scanf("%d" , &a[i]);
+
+    }
+    for(i=0 ; i<n-1 ; i++)
+    {
+        for( j= i+1 ; j<n ; j++)
+        {
+            if(a[i]<a[j])
+            {
+                temp =a[i];
+                a[i] = a[j] ;
+                a[j] = temp;
+            }
+        
+
+        }
+    }
+    printf("Decending order :\n");
+
+    for(i=0 ; i<n ; i++)
+    {
+        printf("%d" , a[i]);
+    }
+    return 0;
+}
