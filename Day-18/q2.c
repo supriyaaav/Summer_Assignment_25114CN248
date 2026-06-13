9ced2b77912f95eefac9c64783d3563ca890b78b
supriyaaav <25114cn248glbitm.ac.in>
2026-06-13 14:07:52 +0530

    updated q2.c

---
+++ Day-18/q2.c
@@ -0,0 +1,41 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,i,j , min;
+
+    printf("Enter size :");
+    scanf("%d" ,&n);
+
+    int a[n];
+
+    printf("Enter element :\n");
+
+    int temp;
+    for(i=0 ; i<n ; i++)
+    {
+        scanf("%d" , &a[i]);
+
+    }
+    for(i=0 ;i<n-1 ; i++ )
+        
+    {
+      min=i;
+        for(j = i+1 ; j<n ; j++)
+        {
+            if(a[j]<a[min])
+            min = j;
+        }
+        temp = a[i];
+        a[i] = a[min];
+        a[min] = temp;
+
+    }
+    printf("Selection sort :\n");
+
+    for(i=0 ; i<n ; i++)
+    {
+        printf("%d" , a[i]);
+    }
+    return 0;
+}
