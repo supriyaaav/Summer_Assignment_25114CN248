639045764f66cc0536c2333c5319d4cf6b987cd8
supriyaaav <25114cn248glbitm.ac.in>
2026-06-14 13:33:28 +0530

    updated q1.c

---
+++ Day-21/q1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    char str[100];
+    int i=0;
+
+    printf("Enter string:");
+    gets(str);
+
+    while(str[i]!= '\0')
+    {
+        i++;
+    }
+    
+    printf("String length : %d"  , i );
+
+    return 0;
+
+}
