59ebf9f1602145108bd46db1ef1df822026a1980
supriyaaav <25114cn248glbitm.ac.in>
2026-06-15 14:03:39 +0530

    updated q1.c

---
+++ Day-23/q1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,count;
+    char str[100];
+
+    printf("Enter string :");
+    gets(str);
+
+        for(i=0 ; str[i]!='\0' ; i++)
+        {
+            count=0;
+            for(j=0 ; str[j]!='\0' ; j++)
+            {
+                if(str[i]==str[j])
+                {
+                   count++;
+                }
+            }
+
+            if(count==1)
+            {
+                printf("First non repeating character : %c",str[i]);
+                break;  
+            }
+        }
+    
+  return 0;
+}
